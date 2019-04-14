9d16ceec6c79cea2d0bfc2e1b5ed515ccc945aa4
asr <ajsoriar@gmail.com>
2019-04-14 03:08:13 +0200

    Upgrade version

--- src/purple-console.js
+++ src/purple-console.js
@@ -1,7 +1,7 @@
 /**
  * purple-console
  * javascript component that prints logs in a DIV layer. It can be useful when dealing with special hardware like Smart TVs or Set-Top Boxes which do not allow debugging.
- * @version v1.0.1 - 2019-02-21
+ * @version v1.1.0 - 2019-04-14
  * @link https://github.com/ajsoriar/purple-console
  * @author Andres J. Soria R. <ajsoriar@gmail.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
